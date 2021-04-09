b3b1796277bab34e4a45878798478451300b67b7
agspf94 <afantin@uolinc.com>
2021-04-09 09:24:37 -0300

    [afantin][bank] Small ajust

--- src/test/kotlin/com/example/simplebank/mock/CustomerApiClientMockStarter.kt
+++ src/test/kotlin/com/example/simplebank/mock/CustomerApiClientMockStarter.kt
@@ -20,7 +20,6 @@ class CustomerApiClientMockStarter {
         )
     }
 
-
     fun stubForValidationDelete(wireMockServer: WireMockServer, status: Int, deleteRequest: DeleteRequest) {
         wireMockServer.stubFor(
             post(urlPathEqualTo("/"))
